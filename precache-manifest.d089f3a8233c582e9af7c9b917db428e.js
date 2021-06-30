b8da7b49e0e784e298d9e03542ca2d03af75fd05
Andrey203 <andrey.dorohov@cruxlab.com>
2021-06-30 16:08:05 +0300

    Updates

---
+++ precache-manifest.d089f3a8233c582e9af7c9b917db428e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd80d62a9d6acd69050bab8412d2e955",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a33b76a2d92dcd2eee7",
+    "url": "./static/css/main.b0179294.chunk.css"
+  },
+  {
+    "revision": "cfed2d8bec0c596d7eed",
+    "url": "./static/js/2.30b0bf7f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.30b0bf7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a33b76a2d92dcd2eee7",
+    "url": "./static/js/main.b2287859.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
